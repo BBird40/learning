c27fd4004374e3a50212bfcf0b6b839781d1751c
BByrd <bryanjebyrd@gmail.com>
2016-05-05 22:37:39 -0500

    Updating app.js to serve static content.

--- node/web-apps/library/app.js
+++ node/web-apps/library/app.js
@@ -5,6 +5,10 @@ var app = express();
 // obvious in production you would do port 80 or 443
 var port = 5000;
 
+// allows us to setup middleware
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
 app.get('/', function(req, res){
     res.send('Hello World');
 });
