60dd21cd0949baf5660a0fa17b8ac95fc60e3e64
BByrd <bryanjebyrd@gmail.com>
2016-05-05 21:32:34 -0500

    Adding Routing and Start script.

--- node/web-apps/library/app.js
+++ node/web-apps/library/app.js
@@ -1,9 +1,18 @@
 var express = require('express');
 
-var hi = 'Hello World!';
+var app = express();
 
-if (true) {
-    console.log('hi')
-}
+// obvious in production you would do port 80 or 443
+var port = 5000;
 
-console.log(hi);
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.get('/auburn', function(req, res){
+    res.send('Hello Auburn');
+});
+
+app.listen(port, function(err){
+    console.log("running server on port " + port + ".");
+});
